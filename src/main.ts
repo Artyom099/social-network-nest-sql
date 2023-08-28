5560374542102d53add1829756ce33175cdc4316
Artyom Golubev <artgolubev@bk.ru>
2023-08-28 22:39:31 +0300

    remove package-lock.json

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,10 @@ export const bootstrap = async () => {
   try {
     const app = await NestFactory.create(AppModule);
     const configService: ConfigService = app.get(ConfigService)
-    const PORT = parseInt(configService.getOrThrow('PORT'), 10)
+    console.log(configService.get(''));
+    console.log(configService.get('port'));
+    console.log(configService.get('PORT'));
+    const PORT = parseInt(configService.getOrThrow<string>('PORT'), 10)
     appSettings(app);
     await app.listen(PORT, () => {
       console.log(`App started at ${PORT} port`);
