d7606994217f23ad0e707148fd54a068b4e50fd8
Artyom Golubev <artgolubev@bk.ru>
2024-03-29 15:09:42 +0300

    refactor

--- src/features/comments/api/comments.controller.ts
+++ src/features/comments/api/comments.controller.ts
@@ -11,44 +11,43 @@ import {
   Put,
   Req,
   UseGuards,
-} from '@nestjs/common';
-import {CommentInputModel} from './models/input/comment.input.model';
-import {BearerAuthGuard} from '../../../infrastructure/guards/bearer-auth.guard';
-import {CheckUserIdGuard} from '../../../infrastructure/guards/check-userId.guard';
-import {CommentsQueryRepository} from '../infrastructure/comments.query.repository';
-import {CommentViewModel} from './models/view/comment.view.model';
-import {LikeStatusInputModel} from './models/input/like.status.input.model';
-import {CommandBus} from '@nestjs/cqrs';
-import {UpdateCommentCommand} from '../application/use.cases/update.comment.use.case';
-import {DeleteCommentCommand} from '../application/use.cases/delete.comment.use.case';
-import {UpdateCommentLikesCommand} from '../application/use.cases/update.comment.likes.use.case';
-import {UsersQueryRepository} from '../../users/infrastructure/users.query.repository';
+} from "@nestjs/common";
+import { CommentInputModel } from "./models/input/comment.input.model";
+import { BearerAuthGuard } from "../../../infrastructure/guards/bearer-auth.guard";
+import { CheckUserIdGuard } from "../../../infrastructure/guards/check-userId.guard";
+import { CommentsQueryRepository } from "../infrastructure/comments.query.repository";
+import { CommentViewModel } from "./models/view/comment.view.model";
+import { LikeStatusInputModel } from "./models/input/like.status.input.model";
+import { CommandBus } from "@nestjs/cqrs";
+import { UpdateCommentCommand } from "../application/use.cases/update.comment.use.case";
+import { DeleteCommentCommand } from "../application/use.cases/delete.comment.use.case";
+import { UpdateCommentLikesCommand } from "../application/use.cases/update.comment.likes.use.case";
+import { UsersQueryRepository } from "../../users/infrastructure/users.query.repository";
 
-
-@Controller('comments')
+@Controller("comments")
 export class CommentsController {
   constructor(
     private commandBus: CommandBus,
     private usersQueryRepository: UsersQueryRepository,
-    private commentsQueryRepository: CommentsQueryRepository,
+    private commentsQueryRepository: CommentsQueryRepository
   ) {}
 
-  @Get(':id')
+  @Get(":id")
   @UseGuards(CheckUserIdGuard)
   @HttpCode(HttpStatus.OK)
   async getComment(
     @Req() req: any,
-    @Param('id') commentId: string,
+    @Param("id") id: string
   ): Promise<CommentViewModel | null> {
     //todo - если юзер забанен, мы не можем получить его коммент - добавить проверку в use case?
     const comment = await this.commentsQueryRepository.getComment(
-      commentId,
-      req.userId,
+      id,
+      req.userId
     );
     if (!comment) throw new NotFoundException();
 
     const user = await this.usersQueryRepository.getUserByIdSA(
-      comment.commentatorInfo.userId,
+      comment.commentatorInfo.userId
     );
     if (user?.banInfo.isBanned) {
       throw new NotFoundException();
@@ -57,62 +56,62 @@ export class CommentsController {
     }
   }
 
-  @Put(':id')
+  @Put(":id")
   @UseGuards(BearerAuthGuard)
   @HttpCode(HttpStatus.NO_CONTENT)
   async updateComment(
     @Req() req: any,
-    @Param('id') commentId: string,
-    @Body() InputModel: CommentInputModel,
+    @Param("id") id: string,
+    @Body() InputModel: CommentInputModel
   ) {
     const comment = await this.commentsQueryRepository.getComment(
-      commentId,
-      req.userId,
+      id,
+      req.userId
     );
     if (!comment) throw new NotFoundException();
+
     if (req.userId !== comment.commentatorInfo.userId) {
       throw new ForbiddenException();
     } else {
-      return this.commandBus.execute(new UpdateCommentCommand(
-        commentId,
-        InputModel.content
-      ));
+      return this.commandBus.execute(
+        new UpdateCommentCommand(id, InputModel.content)
+      );
     }
   }
 
-  @Delete(':id')
+  @Delete(":id")
   @UseGuards(BearerAuthGuard)
   @HttpCode(HttpStatus.NO_CONTENT)
-  async deleteComment(@Req() req: any, @Param('id') commentId: string) {
+  async deleteComment(@Req() req: any, @Param("id") id: string) {
     const comment = await this.commentsQueryRepository.getComment(
-      commentId,
-      req.userId,
+      id,
+      req.userId
     );
     if (!comment) throw new NotFoundException();
+
     if (req.userId !== comment.commentatorInfo.userId) {
       throw new ForbiddenException();
     } else {
-      return this.commandBus.execute(new DeleteCommentCommand(commentId));
+      return this.commandBus.execute(new DeleteCommentCommand(id));
     }
   }
 
-  @Put(':id/like-status')
+  @Put(":id/like-status")
   @UseGuards(BearerAuthGuard)
   @HttpCode(HttpStatus.NO_CONTENT)
   async updateLikeStatus(
     @Req() req: any,
-    @Param('id') commentId: string,
-    @Body() inputModel: LikeStatusInputModel,
+    @Param("id") id: string,
+    @Body() body: LikeStatusInputModel
   ) {
-    const comment = await this.commentsQueryRepository.getComment(commentId);
+    const comment = await this.commentsQueryRepository.getComment(id);
+
     if (!comment) {
       throw new NotFoundException();
     } else {
-      return this.commandBus.execute(new UpdateCommentLikesCommand(
-        commentId,
-        req.userId,
-        inputModel.likeStatus,
-      ));
+      return this.commandBus.execute(
+        new UpdateCommentLikesCommand(id, req.userId, body.likeStatus)
+      );
     }
   }
 }
